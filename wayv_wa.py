4930d61a4ee784d0ad2c853329e1c04938bfe939
inqueue0979 <inqueue0979@gmail.com>
2024-08-22 21:40:45 +0900

    Changed Inner Source

--- wayv_wa.py
+++ wayv_wa.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Hello():
-    return 'WayV WA Server'
+    return 'WayV WA Server | 0822:0940'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
